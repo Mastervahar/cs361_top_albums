f84dfd9423f5fa4d85365a7a1ad286e3c88a88c6
Nathan <hildebrn@oregonstate.edu>
2018-02-14 14:28:52 -0800

    Add heroku git files

--- album_app.rb
+++ album_app.rb
@@ -12,8 +12,10 @@ class AlbumApp
     for album in albums
     	response_body << "
     	<div class=\"name\" style=\"background-color: silver; display: inline-block; width:90%;\">
-    	"+album[0]+"-----"+album[1]+"
-    	</div>"
+    	"+album[0]+"</div>"
+    	response_body << "
+    	<div class=\"year\" style=\"background-color: silver; display: inline-block; width:90%;\">
+    	"+album[1]+"</div>"
     end
     # Append it to the response body.
     response_body << albums.to_s
