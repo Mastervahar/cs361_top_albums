23fb81c0e4358e166f5c4cdb1e4618df72547bd4
Leo G <leo.gothberg@gmail.com>
2018-02-14 15:01:57 -0800

    Implement sort of albums by name, remove garbage second print statement.

--- album_app.rb
+++ album_app.rb
@@ -2,6 +2,10 @@ require 'csv'
 class AlbumApp
 
 	def call(env)
+
+		req = Rack::Request.new(env)
+
+
 		response_body = "
 		<h1>Top 100 Albums of All Time</h1>
 		<div class=\"wrapper\" style=\"display:flex;\">
@@ -13,20 +17,28 @@ class AlbumApp
 		</div>
 		</div>
 		"
+
+
     	# Read the data from the file.
     	# albums = ['fee', 'fi', 'fo', 'funk']
-
     	albums = CSV.read('top_100_albums.txt')
 
+    	# if req.path_info == "/name"
+    	albums = albums.sort_by {|album| album[0]}
+    	# elsif req.path_info == "/year"
+			# albums = albums.sort_by {|album| album[1]}
+		# else
+			# do something else
+		# end
 
-    	for album in albums
-    		response_body << "
-    		<div class=\"name\" style=\"background-color: silver; display: inline-block; width:90%;\">
-    		"+album[0]+"-----"+album[1]+"
-    		</div>"
-    	end
-    # Append it to the response body.
-    response_body << albums.to_s
+		# Append it to the response body.
+		for album in albums
+			response_body << "
+			<div class=\"name\" style=\"background-color: silver; display: inline-block; width:90%;\">
+			"+album[0]+"-----"+album[1]+"
+			</div>"
+		end
+
     # Send the response
     [200, {'Content-Type' => 'text/html'}, [response_body.to_s]]
 end
